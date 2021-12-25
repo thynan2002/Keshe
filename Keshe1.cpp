e2b8ee4552313a831c4378af6fbb6f9d4644989b
西 西 <ghy2012291764@outlook.com>
2021-12-25 17:50:01 +0800

    修改

--- Keshe1.cpp
+++ Keshe1.cpp
@@ -4,24 +4,24 @@
 #include <string>
 #include <vector>
 #include <cassert>
-#include<chrono>
-#include<thread>
+#include<iomanip>
 #define ComN 200				//公司数量
 using namespace std;
 
 /*股票详细数据*/
-typedef struct {
-	string date;				//日期
-	string oprice;				//开盘价
-	string cprice;				//收盘价
-	string maxprice;			//最高价
-	string minprice;			//最低价
-	string volume;				//成交量
-	string turnover;			//成交额
+typedef struct detail{
+	unsigned long long date;				//日期
+	double oprice;				//开盘价
+	double cprice;				//收盘价
+	double maxprice;			//最高价
+	double minprice;			//最低价
+	double volume;				//成交量
+	double turnover;			//成交额
 	string rate;				//换手率
-	string iodamount;			//涨跌额
+	double iodamount;			//涨跌额
 	string iodrate;				//涨跌幅
-}detail;
+	struct detail* next;
+}detail,*det;
 typedef struct{
 	string StockNum;			//股票代码
 	string StockName;			//股票简称
@@ -40,8 +40,9 @@ typedef struct{
 	string StockTel;			//电话
 	string StockMB;				//主营业务 Main Business
 	string StockBS;				//经营范围 Business Scope
-	detail* details;			//详细信息
+	det details;			//详细信息_最近日期
 }stock;
+
 typedef struct LNode{
 	stock data;
 	struct LNode* next;
@@ -57,12 +58,11 @@ LinkList Array[97];
 void MainMenu(stock stocks[]);
 void Welcome(stock stocks[]);
 void SearchMenu(stock stocks[]);
-//void AnalyzeMenu(stock stocks[]);
+void AnalyzeMenu(stock stocks[]);
 void S_StockInfo(stock stocks[]);
 
 
-
-void Read(stock stocks[]) {
+void ReadCSV(stock stocks[]) {
 	ifstream inFile1("A股公司简介.csv", ios::in);
 	string lineStr1;
 	getline(inFile1, lineStr1);
@@ -95,7 +95,9 @@ void Read(stock stocks[]) {
 		stocks[i].StockBS = str1[16];
 		i++;
 	}
-	for (int m = 0; m < ComN; m++) {
+
+	//ReadTxt
+	/*for (int m = 0; m < ComN; m++) {
 		string file_name = stocks[m].StockNum + ".txt";
 		ifstream inFile(file_name, ios::in);
 		string lineStr;
@@ -105,10 +107,11 @@ void Read(stock stocks[]) {
 			stringstream ss(lineStr);
 			string str[200];
 			int j = 0;
+			
 			while (getline(ss, str[j], ' ')) {
 				j++;
 			}
-			stocks[m].details = new detail[200];
+			stocks[m].details = new detail[999];
 			stocks[m].details[k].date = str[0];
 			stocks[m].details[k].oprice = str[1];
 			stocks[m].details[k].cprice = str[2];
@@ -123,9 +126,37 @@ void Read(stock stocks[]) {
 			break;
 		}
 
-	}
-	//string FileName = ""
+	}*/
+
 }
+
+void ReadTXT(stock stocks[]) {
+	string k = "股票涨跌数据/";
+	unsigned long long as;
+	string ss;
+	for (int i = 0; i < ComN; i++)
+	{
+		stocks[i].details = new detail;
+		stocks[i].details->next = NULL;
+		det temp, p = stocks[i].details;
+		//k = "股票涨跌数据/";
+		k += stocks[i].StockNum + ".txt";
+		ifstream infile(k);
+		for (int j = 0; j < 10; j++) infile >> ss;//读表头
+		while (infile >> as) {
+			temp = new detail;
+			temp->date = as;
+			infile >> temp->oprice >> temp->cprice >> temp->maxprice >> temp->minprice >> temp->volume >> temp->turnover >> temp->rate >> temp->iodamount >> temp->iodrate;
+			temp->next = NULL;
+			p->next = temp;
+			p = p->next;
+			cout << temp->date << " " <<temp->oprice <<" "<< temp->cprice << " " << temp->maxprice << " " << temp->minprice << " " << temp->volume << " " << temp->turnover << " " << temp->rate<<" " << temp->iodamount << " " << temp->iodrate << " " << endl;
+		}
+		infile.close();
+	}
+}
+
+
 
 
 /***********    Search    ************/
@@ -240,7 +271,7 @@ void S_SI_BT(stock stocks[]) {					//查询_股票网址查询_基于二叉树BT
 	if (b != NULL) {
 		cout << "查找成功，详情如下：" << endl;
 		cout << b->data.StockNum << "\t" << b->data.StockName << "\t" << endl;
-		cout << b->data.details->oprice << "\t" << b->data.details->cprice << "\t" << b->data.details->iodrate << endl;
+		cout <<setiosflags(ios::fixed)<< b->data.details->next->oprice<< "\t" << setiosflags(ios::fixed) << b->data.details->next->cprice << "\t" << b->data.details->next->iodrate << endl;
 	}
 	else {
 		cout << "未找到相关信息，请确认输入信息有效" << endl;
@@ -322,7 +353,19 @@ void S_StockPrice(stock stocks[]) {				//查询_股票的价格信息
 	cout << "**********************************" << endl;
 	cout << "*这是一个基于单链表的股票价格查询*" << endl;
 	cout << "**********************************" << endl;
-	cout << "查询结果如下：" << endl << endl << endl;
+	cout << "请输入你要查找的日期:";
+	unsigned long long tobes; cin >> tobes;
+	for (int i = 0; i < ComN; i++) {
+		det p = stocks[i].details;
+		while (p) {
+			if (tobes == p->date) {
+				cout << stocks[i].StockNum << "\t" << stocks[i].StockName << "\t" <<setiosflags(ios::fixed)<< stocks[i].details->next->oprice << "\t" << stocks[i].details->next->cprice << "\t" << stocks[i].details->next->iodrate << endl;
+			}
+			else {
+				p = p->next;
+			}
+		}
+	}
 	cout << "即将进入到上一级菜单" << endl; system("pause");
 	SearchMenu(stocks);
 }
@@ -356,135 +399,135 @@ void SearchMenu(stock stocks[]) {					//查询
 }
 /***********    Analyze    ************/
 //股票价格分析
-//void A_StockPrice_QS(stock stocks[]) {			//分析_股票价格分析_快速排序QuickS
-//	cout << "************************************" << endl;
-//	cout << "*这是一个基于快速排序的股票价格分析*" << endl;
-//	cout << "************************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl; system("pause");
-//	AnalyzeMenu();
-//
-//}
-//void A_StockPrice_DIS(stock stocks) {			//分析_股票价格分析_基于直接插入排序DirectInsertS
-//	cout << "****************************************" << endl;
-//	cout << "*这是一个基于直接插入排序的股票价格分析*" << endl;
-//	cout << "****************************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl; system("pause");
-//	AnalyzeMenu();
-//
-//}
-//void A_StockPrice_SS(stock stocks[]) {			//分析_股票价格分析_基于简单选择排序SelectionS
-//	cout << "****************************************" << endl;
-//	cout << "*这是一个基于简单选择排序的股票价格分析*" << endl;
-//	cout << "****************************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl; system("pause");
-//	AnalyzeMenu();
-//
-//}
-//void A_StockPrice(stock stocks[]) {				//分析_股票价格分析
-//	cout << "分析方式有" << endl;
-//	cout << "1—基于快速排序 || 2—基于直接插入排序 || 3—基于简单选择排序" << endl;
-//	cout << "请选择：";
-//	int ToDo = -1; cin >> ToDo;
-//	if (ToDo == 1) {
-//		A_StockPrice_QS();
-//	}
-//	else if (ToDo == 2) {
-//		A_StockPrice_DIS();
-//	}
-//	else if (ToDo == 3) {
-//		A_StockPrice_SS();
-//	}
-//	else {
-//		cout << "请输入有效选择项！" << endl;
-//		system("pause");
-//		cout << endl;
-//		A_StockPrice();
-//	}
-//}
-////股票相关性分析
-//void A_StockRelevance(stocks) {			//分析_股票相关性分析-基于Floyd
-//	cout << "***********************************" << endl;
-//	cout << "*这是一个基于Floyd的股票相关性分析*" << endl;
-//	cout << "***********************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl; system("pause");
-//	AnalyzeMenu();
-//
-//}
-////股票基金筛选
-//void A_StockFund_Prim(stocks) {			//分析_股票基金筛选_基于Prime最小生成树
-//	cout << "******************************************" << endl;
-//	cout << "*这是一个基于Prim最小生成树的股票基金筛选*" << endl;
-//	cout << "******************************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl; system("pause");
-//	AnalyzeMenu();
-//
-//}
-//void A_StockFund_Bigraph(stocks) {		//分析_股票基金筛选_基于二部图Bigraph
-//	cout << "**********************************" << endl;
-//	cout << "*这是一个基于二部图的股票基金筛选*" << endl;
-//	cout << "**********************************" << endl;
-//
-//	cout << "分析结果如下：" << endl << endl << endl;
-//	cout << "即将进入到上一级菜单" << endl;	system("pause");
-//	AnalyzeMenu();
-//
-//}
-//void A_StockFund(stocks) {					//分析_股票基金筛选
-//	cout << "分析方式有：" << endl;
-//	cout << "1—基于Prim最小生成树 || 2—基于二部图" << endl;
-//	cout << "请选择：";
-//	int ToDo = -1; cin >> ToDo;
-//	if (ToDo == 1) {
-//		A_StockFund_Prim();
-//	}
-//	else if (ToDo == 2) {
-//		A_StockFund_Bigraph();
-//	}
-//	else {
-//		cout << "请输入有效选择项!" << endl;
-//		system("pause");
-//		cout << endl;
-//		A_StockFund();
-//	}
-//}
-////分析二级菜单
-//void AnalyzeMenu(stocks) {				//分析
-//	cout << endl << "可进行的操作有:" << endl;
-//	cout << "1——股票价格分析" << endl;
-//	cout << "2——股票相关性计算分析" << endl;
-//	cout << "3——股票基金筛选" << endl;
-//	cout << "4——返回到主页面菜单" << endl;
-//	cout << "请选择:";
-//
-//	int ToDo = -1;
-//	cin >> ToDo;
-//	if (ToDo == 1) {
-//		A_StockPrice();
-//	}
-//	else if (ToDo == 2) {
-//		A_StockRelevance();
-//	}
-//	else if (ToDo == 3) {
-//		A_StockFund();
-//	}
-//	else if (ToDo == 4) {
-//		MainMenu();
-//	}
-//	else {
-//		cout << "请输入有效选择项!" << endl;
-//		AnalyzeMenu();
-//	}
-//}
+void A_StockPrice_QS(stock stocks[]) {			//分析_股票价格分析_快速排序QuickS
+	cout << "************************************" << endl;
+	cout << "*这是一个基于快速排序的股票价格分析*" << endl;
+	cout << "************************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl; system("pause");
+	AnalyzeMenu(stocks);
+
+}
+void A_StockPrice_DIS(stock stocks[]) {			//分析_股票价格分析_基于直接插入排序DirectInsertS
+	cout << "****************************************" << endl;
+	cout << "*这是一个基于直接插入排序的股票价格分析*" << endl;
+	cout << "****************************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl; system("pause");
+	AnalyzeMenu(stocks);
+
+}
+void A_StockPrice_SS(stock stocks[]) {			//分析_股票价格分析_基于简单选择排序SelectionS
+	cout << "****************************************" << endl;
+	cout << "*这是一个基于简单选择排序的股票价格分析*" << endl;
+	cout << "****************************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl; system("pause");
+	AnalyzeMenu(stocks);
+
+}
+void A_StockPrice(stock stocks[]) {				//分析_股票价格分析
+	cout << "分析方式有" << endl;
+	cout << "1—基于快速排序 || 2—基于直接插入排序 || 3—基于简单选择排序" << endl;
+	cout << "请选择：";
+	int ToDo = -1; cin >> ToDo;
+	if (ToDo == 1) {
+		A_StockPrice_QS(stocks);
+	}
+	else if (ToDo == 2) {
+		A_StockPrice_DIS(stocks);
+	}
+	else if (ToDo == 3) {
+		A_StockPrice_SS(stocks);
+	}
+	else {
+		cout << "请输入有效选择项！" << endl;
+		system("pause");
+		cout << endl;
+		A_StockPrice(stocks);
+	}
+}
+//股票相关性分析
+void A_StockRelevance(stock stocks[]) {			//分析_股票相关性分析-基于Floyd
+	cout << "***********************************" << endl;
+	cout << "*这是一个基于Floyd的股票相关性分析*" << endl;
+	cout << "***********************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl; system("pause");
+	AnalyzeMenu(stocks);
+
+}
+//股票基金筛选
+void A_StockFund_Prim(stock stocks[]) {			//分析_股票基金筛选_基于Prime最小生成树
+	cout << "******************************************" << endl;
+	cout << "*这是一个基于Prim最小生成树的股票基金筛选*" << endl;
+	cout << "******************************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl; system("pause");
+	AnalyzeMenu(stocks);
+
+}
+void A_StockFund_Bigraph(stock stocks[]) {		//分析_股票基金筛选_基于二部图Bigraph
+	cout << "**********************************" << endl;
+	cout << "*这是一个基于二部图的股票基金筛选*" << endl;
+	cout << "**********************************" << endl;
+
+	cout << "分析结果如下：" << endl << endl << endl;
+	cout << "即将进入到上一级菜单" << endl;	system("pause");
+	AnalyzeMenu(stocks);
+
+}
+void A_StockFund(stock stocks[]) {					//分析_股票基金筛选
+	cout << "分析方式有：" << endl;
+	cout << "1—基于Prim最小生成树 || 2—基于二部图" << endl;
+	cout << "请选择：";
+	int ToDo = -1; cin >> ToDo;
+	if (ToDo == 1) {
+		A_StockFund_Prim(stocks);
+	}
+	else if (ToDo == 2) {
+		A_StockFund_Bigraph(stocks);
+	}
+	else {
+		cout << "请输入有效选择项!" << endl;
+		system("pause");
+		cout << endl;
+		A_StockFund(stocks);
+	}
+}
+//分析二级菜单
+void AnalyzeMenu(stock stocks[]) {				//分析
+	cout << endl << "可进行的操作有:" << endl;
+	cout << "1——股票价格分析" << endl;
+	cout << "2——股票相关性计算分析" << endl;
+	cout << "3——股票基金筛选" << endl;
+	cout << "4——返回到主页面菜单" << endl;
+	cout << "请选择:";
+
+	int ToDo = -1;
+	cin >> ToDo;
+	if (ToDo == 1) {
+		A_StockPrice(stocks);
+	}
+	else if (ToDo == 2) {
+		A_StockRelevance(stocks);
+	}
+	else if (ToDo == 3) {
+		A_StockFund(stocks);
+	}
+	else if (ToDo == 4) {
+		MainMenu(stocks);
+	}
+	else {
+		cout << "请输入有效选择项!" << endl;
+		AnalyzeMenu(stocks);
+	}
+}
 
 /***********    主菜单    ************/
 void MainMenu(stock stocks[]) {				//主菜单
@@ -504,8 +547,8 @@ void MainMenu(stock stocks[]) {				//主菜单
 		SearchMenu(stocks);
 	}
 	else if (ToDo == 2) {
-		MainMenu(stocks);
-		//AnalyzeMenu(stocks);
+		//MainMenu(stocks);
+		AnalyzeMenu(stocks);
 	}
 	else if (ToDo == 3) {
 		exit(0);
@@ -534,7 +577,8 @@ int main()
 {
 	stock stocks[201];
 
-	Read(stocks);
+	ReadCSV(stocks);
+	ReadTXT(stocks);
 	CreatHash(stocks);
 	Welcome(stocks);
 
